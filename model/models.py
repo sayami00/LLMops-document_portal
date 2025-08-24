a535d32c61ccd396e0b1652d627360986cb90c3f
Ramesh Manandhar <sayami00@gmail.com>
2025-08-24 21:35:56 +0545

    commit_before_deployment

--- model/models.py
+++ model/models.py
@@ -1,26 +1,24 @@
-from pydantic import BaseModel,RootModel
-from typing import List,Union
+from pydantic import BaseModel, RootModel
+from typing import List, Union
 from enum import Enum
 
 class Metadata(BaseModel):
-    Summary : List[str]
-    Title : str
-    Auther : List[str]
-    DateCreated:str
-    LastModifiedDate:str
-    Publisher:str
-    Language:str
-    PageCount: Union[int,str] #Can be :Not Available"
-    SentimentTone:str
-
+    Summary: List[str]
+    Title: str
+    Author: List[str]
+    DateCreated: str
+    LastModifiedDate: str
+    Publisher: str
+    Language: str
+    PageCount: Union[int, str]  # Can be "Not Available"
+    SentimentTone: str
 class ChangeFormat(BaseModel):
-    Page:str
-    Changes:str
+    Page: str
+    Changes: str
 
 class SummaryResponse(RootModel[list[ChangeFormat]]):
     pass
-
-class PromptType(str,Enum):
+class PromptType(str, Enum):
     DOCUMENT_ANALYSIS = "document_analysis"
     DOCUMENT_COMPARISON = "document_comparison"
     CONTEXTUALIZE_QUESTION = "contextualize_question"
